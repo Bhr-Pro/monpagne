05bf472f0bf2ac838dec87035caae47e14439b2d
wossana <wossana1988@gmail.com>
2019-02-19 12:58:21 +0000

    ajouter les fichier

---
+++ afficher.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style.css" />
+        <title>acccueil</title>
+    </head>
+
+    <body>
+        <header>
+            
+            <h2>enregitrer vous!</h2>
+        </header>
+        
+         <section>
+        </section>
+        <p>
+je m'appele nom:<?php echo $_POST['nom'] ?></br> et prenom: <?php echo $_POST['prenom'] ?> </br> 
+ j'ai <?php echo $_POST['age'] ?> </br> 
+
+<p>Si tu veux changer de prénom, <a href="ex.php">clique ici</a> pour revenir à la page ex.php.</p>
+
+        <footer>
+            <p>Copyright wossana- Tous droits réservés<br />
+
+           
+        </footer>
+        
+    </body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ex.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style.css" />
+        <title>acccueil</title>
+    </head>
+
+    <body>
+        <header>
+            
+            <h2>enregitrer vous!</h2>
+        </header>
+        
+       
+        <form action="afficher.php"   method="post" >
+         
+         NOM:  <input type="text" name="nom" /></br>
+          PRENOM: <input type="text" name="prenom"/></br>
+         AGE: <input type="text" name="age" /></br>
+           <input type="submit" value="envoyer" value="presenter"/>
+
+        </form>
+        
+        <footer>
+            <p>Copyrightwossana- Tous droits réservés<br />
+
+           
+        </footer>
+        
+    </body>
+</html>
